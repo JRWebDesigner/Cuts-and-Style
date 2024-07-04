1594580e8e89eef8820c28d8aba0ca2bcd679963
josuem317 <jowebdesigc@gmail.com>
2024-07-03 22:25:55 -0400

    inicio de pagina para mamá

---
+++ src/components/Testimony.jsx
@@ -0,0 +1,16 @@
+import { map } from "astro/zod"
+
+let users={
+    nombre:[
+        "Susana",
+        "Cristian",
+        "Josue",
+        "Cristian",
+    ],
+    comentario:[
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptates eos voluptatibus assumenda pariatur ad, voluptas repellat iusto ullam quia voluptate fugit saepe magnam fugiat nisi at quos, officiis dolores.",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptates eos voluptatibus assumenda pariatur ad, voluptas repellat iusto ullam quia voluptate fugit saepe magnam fugiat nisi at quos, officiis dolores.",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptates eos voluptatibus assumenda pariatur ad, voluptas repellat iusto ullam quia voluptate fugit saepe magnam fugiat nisi at quos, officiis dolores.",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptates eos voluptatibus assumenda pariatur ad, voluptas repellat iusto ullam quia voluptate fugit saepe magnam fugiat nisi at quos, officiis dolores.",
+    ]
+}
